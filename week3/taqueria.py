2d1160f1a0ccccbf20b8d9ac0353ae474f1ababc
Abdullah Khalid <abdullah.khalid8970@gmail.com>
2024-07-17 14:49:43 -0400

    Create taqueria.py

---
+++ week3/taqueria.py
@@ -0,0 +1,35 @@
+menu = {
+  "Baja Taco": 4.25,
+  "Burrito": 7.50,
+  "Bowl": 8.50,
+  "Nachos": 11.00,
+  "Quesadilla": 8.50,
+  "Super Burrito": 8.50,
+  "Super Quesadilla": 9.50,
+  "Taco": 3.00,
+  "Tortilla Salad": 8.00
+}
+
+def main():
+  counter = 0
+  while True:
+    try:
+      x = convert("Item: ")
+      counter += x
+      print(f"${counter:.2f}")
+    except EOFError:
+      print()
+      exit()
+
+
+
+def convert(text):
+  while True:
+    try:
+      item = input(text).title()
+      if item in menu:
+        return menu[item]
+    except KeyError:
+      pass
+
+main()
