c5bc148ea239663a34f84cc5b91009f18ee0d78c
Abdullah Khalid <abdullah.khalid8970@gmail.com>
2024-07-17 15:11:20 -0400

    Delete week8/seasons/test_season.py

--- week8/seasons/test_season.py
+++
@@ -1,8 +0,0 @@
-from seasons import convert
-import pytest
-
-def test_function():
-    assert convert("2007-05-10") == "Eight million, nine hundred thirteen thousand, six hundred minutes"
-    assert convert("1987-06-21") == "Nineteen million, three hundred seventy-two thousand, three hundred twenty minutes"
-    with pytest.raises(SystemExit):
-        assert convert("May 6, 1767")
