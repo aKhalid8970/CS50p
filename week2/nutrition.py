b717f5e16fda319c48bf690097078942bd8697c1
Abdullah Khalid <abdullah.khalid8970@gmail.com>
2024-07-17 14:47:20 -0400

    Create nutrition.py

---
+++ week2/nutrition.py
@@ -0,0 +1,26 @@
+items = {
+    "apple": "130",
+    "avocado": "50",
+    "banana": "110",
+    "cantaloupe": "50",
+    "grapefruit": "60",
+    "honeydew melon": "50",
+    "kiwifruit": "90",
+    "lemon": "15",
+    "lime": "20",
+    "nectarine": "60",
+    "orange": "80",
+    "peach": "60",
+    "pear": "100",
+    "pineapple": "50",
+    "plums": "70",
+    "strawberries": "50",
+    "sweet cherries": "100",
+    "tangerine": "50",
+    "watermelon": "80",
+    "grapes": "90",
+}
+
+item = input("Item: ").lower()
+if item in items:
+    print(f"Calories: {items[item]}")
